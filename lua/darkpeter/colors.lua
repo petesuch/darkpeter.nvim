63cde6dab63ecb0f2cf682993b3d1652aab625eb
Peter Suchsland <visiondoctor2020@gmail.com>
2025-02-26 15:54:24 +0700

    Update colors.lua
    just changing what needs to changed

--- lua/darkpeter/colors.lua
+++ lua/darkpeter/colors.lua
@@ -1,9 +1,9 @@
-local colors = require("onedark.palette")
+local colors = require("darkpeter.palette")
 
 local function select_colors()
 	local selected = { none = "none" }
-	selected = vim.tbl_extend("force", selected, colors[vim.g.onedark_config.style])
-	selected = vim.tbl_extend("force", selected, vim.g.onedark_config.colors)
+	selected = vim.tbl_extend("force", selected, colors[vim.g.darkpeter_config.style])
+	selected = vim.tbl_extend("force", selected, vim.g.darkpeter_config.colors)
 	return selected
 end
 
