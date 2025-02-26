59e3591ce782269037d35d89a8ad7247855fc382
Peter Suchsland <visiondoctor2020@gmail.com>
2025-02-26 15:36:39 +0700

    Update onedark.lua

--- colors/onedark.lua
+++ colors/onedark.lua
@@ -1,6 +1,6 @@
 for k in pairs(package.loaded) do
-    if k:match(".*onedark.*") then package.loaded[k] = nil end
+    if k:match(".*darkpeter.*") then package.loaded[k] = nil end
 end
 
-require('onedark').setup()
-require('onedark').colorscheme()
+require('darkpeter').setup()
+require('darkpeter').colorscheme()
