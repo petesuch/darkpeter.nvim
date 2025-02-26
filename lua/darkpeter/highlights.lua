21abc6bb575d4fb17ec0fe73b890a5b0dbf94c6e
Peter Suchsland <visiondoctor2020@gmail.com>
2025-02-26 15:57:27 +0700

    Update highlights.lua

--- lua/darkpeter/highlights.lua
+++ lua/darkpeter/highlights.lua
@@ -926,7 +926,7 @@ function M.setup()
             color_name = c[name]
             if not color_name then
                 vim.schedule(function()
-                    vim.notify('onedark.nvim: unknown color "' .. name .. '"', vim.log.levels.ERROR, { title = "onedark.nvim" })
+                    vim.notify('.nvim: unknown color "' .. name .. '"', vim.log.levels.ERROR, { title = "darkpeter.nvim" })
                 end)
                 return ""
             end
@@ -934,7 +934,7 @@ function M.setup()
         return prefix .. "=" .. color_name
     end
 
-    for group_name, group_settings in pairs(vim.g.onedark_config.highlights) do
+    for group_name, group_settings in pairs(vim.g.darkpeter_config.highlights) do
         vim.api.nvim_command(string.format("highlight %s %s %s %s %s", group_name,
             replace_color("guifg", group_settings.fg),
             replace_color("guibg", group_settings.bg),
